0b2828386cfe6deee16aa7a0d3b36cc05667424c
Dean Liu <deanliu@gmail.com>
2016-12-09 07:22:45 -0800

    Turning angle of warp

--- drawwarped.py
+++ drawwarped.py
@@ -7,6 +7,15 @@ from warped import Warped
 from skimage.color import rgb2grey
 from skimage import io, feature
 import matplotlib.pyplot as plt
+import numpy as np
+from enum import Enum
+
+class Slope(Enum):
+    uphill = 1
+    downhill = 2
+    horizontal = 3
+    vertical = 4
+    
 
 class DrawWarped():
 
@@ -15,12 +24,28 @@ class DrawWarped():
         self.image = Image.new('RGB', self.og_image.size)
         self.draw = ImageDraw.Draw(self.image, 'RGBA')
 
-
         grey_img = rgb2grey(io.imread(imgpath))
         self.img_edges = feature.canny(grey_img, sigma=3)
 
-        plt.imshow(self.img_edges, cmap=plt.cm.gray)
-        plt.show()
+        # plt.imshow(self.img_edges, cmap=plt.cm.gray)
+        # plt.show()
+
+    def get_slope(self, img_seg):
+        # if len(img_seg[img_seg==True]) > len(img_seg[img_seg==False])/50:
+        if len(img_seg[img_seg==True]):
+            x,y = np.where(img_seg==True)
+
+            slope,_ = np.polyfit(x,y,1)
+            return slope
+
+            if slope >= 0:
+                return Slope.downhill
+            elif slope < 0:
+                return Slope.uphill
+            # else:
+            #     return Slope.horizontal
+
+
 
     def warp(self):
         width,height = self.image.size
@@ -28,7 +53,7 @@ class DrawWarped():
         print width,height
         pix = 35
 
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
         for w in range(width/pix):
             for h in range(height/pix):
@@ -37,24 +62,37 @@ class DrawWarped():
                 x,y = w*pix,h*pix
                 rect_coords = [x,y,x+pix, y+pix]
 
-
+                slope = self.get_slope(self.img_edges[y:y+pix,x:x+pix])
 
                 color = util.average_color(self.og_image, rect=rect_coords)
+                if slope and not np.isnan(slope):
+                    print [x,y,x+pix, y+pix]
+                    if slope < 0: # uphill
+                        color = (200,0,0)
+                    if slope >= 0: # downhill
+                        color = (0,255,0)
+                    # import pdb; pdb.set_trace()
+                    warped_rect = Warped(size=(pix,pix), color=color)
+                    img = warped_rect.draw(slope)
+                    self.image.paste(img, (w*pix,h*pix))
+                    self.og_image.paste(img, (w*pix,h*pix))
 
-                warped_rect = Warped(size=(pix,pix), color=color)
-                img = warped_rect.draw()
+                    # self.og_image.show()
 
-                # self.draw.rectangle(shape.coords(), fill=color)
-                self.image.paste(img, (w*pix,h*pix))
-                # self.image.show()
-                self.og_image.paste(img, (w*pix,h*pix))
 
+                # warped_rect = Warped(size=(pix,pix), color=color)
+                # img = warped_rect.draw()
+
+                # # self.draw.rectangle(shape.coords(), fill=color)
+                # self.image.paste(img, (w*pix,h*pix))
+                # # self.image.show()
+                # self.og_image.paste(img, (w*pix,h*pix))
+
+                
+                # print w,h
+
+            # if w%10:
                 # self.og_image.show()
-                # import pdb; pdb.set_trace()
-                print w,h
-
-            if w%10:
-                self.og_image.show()
 
 
         self.og_image.show()
--- warped.py
+++ warped.py
@@ -28,12 +28,117 @@ class Warped():
         self.num_circles = 4
         self.warp_height = .04*np.random.random()
 
-        self.sincos = np.cos if bool(random.getrandbits(1)) else np.sin
+        self.sincos = np.sin#np.cos if bool(random.getrandbits(1)) else np.sin
 
     def __paint_circles(self):
         pass
 
-    def draw(self):
+
+    # def draw(self):
+    #     show_image = False
+
+    #     is_horizontal = True if self.width >= self.height else False
+
+    #     if is_horizontal:
+    #         ratio = self.width / float(self.height)
+    #     else:
+    #         ratio = self.height / float(self.width)
+
+    #     CIRCLE_SIZE = max(self.width, self.height)
+        
+    #     print "circle size", CIRCLE_SIZE
+
+    #     circle_img = Image.new('RGBA', (CIRCLE_SIZE,CIRCLE_SIZE))
+
+    #     canvas = ImageDraw.Draw(circle_img)
+
+    #     csize = CIRCLE_SIZE/(self.num_circles*2)
+
+    #     color0 = self.color #(100,100,200)
+    #     color1, color2 = util.adjacent_colors(color0)
+    #     color3, color4 = util.adjacent_colors(color1)
+    #     color4, color5 = util.adjacent_colors(color2)
+    #     colors = [color0, color1, color2, color3, color4, color5]
+
+    #     shuffle(colors)
+    #     # print colors
+
+    #     paper = Image.new('RGBA', (CIRCLE_SIZE,CIRCLE_SIZE))
+    #     paper.paste(colors[0], [0,0,self.width,self.height])
+
+    #     canvas.ellipse([0, 0, CIRCLE_SIZE,CIRCLE_SIZE], fill=colors[1])
+    #     canvas.ellipse([csize, csize, CIRCLE_SIZE-csize, CIRCLE_SIZE-csize], fill=colors[2])
+    #     canvas.ellipse([csize*2, csize*2, CIRCLE_SIZE-csize*2, CIRCLE_SIZE-csize*2], fill=colors[3])
+    #     canvas.ellipse([csize*3, csize*3, CIRCLE_SIZE-csize*3, CIRCLE_SIZE-csize*3], fill=colors[4])
+
+    #     # paper = Image.new('RGBA', (CIRCLE_SIZE,CIRCLE_SIZE))
+    #     # paper.paste(color0, [0,0,self.width,self.height])
+    #     # canvas.ellipse([0, 0, CIRCLE_SIZE,CIRCLE_SIZE], fill=color1)
+    #     # canvas.ellipse([csize, csize, CIRCLE_SIZE-csize, CIRCLE_SIZE-csize], fill=color2)
+    #     # canvas.ellipse([csize*2, csize*2, CIRCLE_SIZE-csize*2, CIRCLE_SIZE-csize*2], fill=color3)
+    #     # canvas.ellipse([csize*3, csize*3, CIRCLE_SIZE-csize*3, CIRCLE_SIZE-csize*3], fill=color4)
+
+
+    #     if show_image:
+    #         circle_img.show()
+    #     image = np.asarray(circle_img)
+    #     # rows, cols = image.shape[0], image.shape[1]
+    #     rows, cols = self.height, self.width
+
+    #     src_cols = np.linspace(0, cols, 50)
+    #     src_rows = np.linspace(0, rows, 50)
+    #     src_rows, src_cols = np.meshgrid(src_rows, src_cols)
+    #     src = np.dstack([src_cols.flat, src_rows.flat])[0]
+
+    #     # add sinusoidal oscillation to row coordinates
+    #     HEIGHT_OSC = CIRCLE_SIZE * self.warp_height
+
+    #     if is_horizontal:
+    #         dst_rows = src[:, 1] - self.sincos(np.linspace(0, 4 * np.pi, src.shape[0])) * HEIGHT_OSC
+    #         dst_cols = src[:, 0] 
+
+    #         dst_rows *= (ratio+self.warp_height)
+    #         dst_rows -= ratio * HEIGHT_OSC # subtract y position to account for mutating up
+    #     else:
+    #         dst_rows = src[:, 1] 
+    #         dst_cols = src[:, 0]  - self.sincos(np.linspace(0, 3 * np.pi, src.shape[0])) * HEIGHT_OSC
+
+    #         dst_cols *= (ratio+self.warp_height)
+    #         # dst_cols -= ratio * HEIGHT_OSC # subtract y position to account for mutating up
+
+    #     dst = np.vstack([dst_cols, dst_rows]).T
+
+    #     tform = PiecewiseAffineTransform()
+    #     tform.estimate(src, dst)
+
+    #     out_rows = rows #image.shape[0] - 1.5 * HEIGHT_OSC
+    #     out_cols = cols
+
+    #     print out_cols, out_rows
+    #     out = warp(image, tform, output_shape=(out_rows, out_cols))
+
+    #     # fig, ax = plt.subplots()
+
+
+    #     # if show_image:
+    #     #     ax.imshow(out)
+    #     #     # ax.plot(tform.inverse(src)[:, 0], tform.inverse(src)[:, 1], '.b') # plots the dots
+    #     #     ax.axis((0, out_cols, out_rows, 0))
+    #     #     plt.show()
+
+    #     converted = scipy.misc.toimage(out)
+
+    #     # import pdb; pdb.set_trace()
+    #     # paper.paste(converted, (0,0))
+    #     paper.paste(converted, (0, 0), converted)
+    #     # if show_image:
+    #     #     converted.show()
+
+    #     return paper
+
+
+
+    def draw(self, slope=1):
         show_image = False
 
         is_horizontal = True if self.width >= self.height else False
@@ -53,14 +158,14 @@ class Warped():
 
         csize = CIRCLE_SIZE/(self.num_circles*2)
 
-        color0 = self.color #(100,100,200)
+        color0 = self.color # (100,100,200)
         color1, color2 = util.adjacent_colors(color0)
         color3, color4 = util.adjacent_colors(color1)
         color4, color5 = util.adjacent_colors(color2)
         colors = [color0, color1, color2, color3, color4, color5]
 
         shuffle(colors)
-        print colors
+        # print colors
 
         paper = Image.new('RGBA', (CIRCLE_SIZE,CIRCLE_SIZE))
         paper.paste(colors[0], [0,0,self.width,self.height])
@@ -77,9 +182,8 @@ class Warped():
         # canvas.ellipse([csize*2, csize*2, CIRCLE_SIZE-csize*2, CIRCLE_SIZE-csize*2], fill=color3)
         # canvas.ellipse([csize*3, csize*3, CIRCLE_SIZE-csize*3, CIRCLE_SIZE-csize*3], fill=color4)
 
-
-        if show_image:
-            circle_img.show()
+        # if show_image:
+        #     circle_img.show()
         image = np.asarray(circle_img)
         # rows, cols = image.shape[0], image.shape[1]
         rows, cols = self.height, self.width
@@ -93,11 +197,12 @@ class Warped():
         HEIGHT_OSC = CIRCLE_SIZE * self.warp_height
 
         if is_horizontal:
-            dst_rows = src[:, 1] - self.sincos(np.linspace(0, 4 * np.pi, src.shape[0])) * HEIGHT_OSC
+            dst_rows = src[:, 1] - self.sincos(np.linspace(0, .9 * np.pi, src.shape[0])) * HEIGHT_OSC
             dst_cols = src[:, 0] 
 
-            dst_rows *= (ratio+self.warp_height)
-            dst_rows -= ratio * HEIGHT_OSC # subtract y position to account for mutating up
+            dst_rows *= (ratio+self.warp_height+3)
+            # dst_rows -=  800#* (ratio+self.warp_height)
+            # dst_rows -= (ratio * HEIGHT_OSC) # subtract y position to account for mutating up
         else:
             dst_rows = src[:, 1] 
             dst_cols = src[:, 0]  - self.sincos(np.linspace(0, 3 * np.pi, src.shape[0])) * HEIGHT_OSC
@@ -115,23 +220,33 @@ class Warped():
 
         print out_cols, out_rows
         out = warp(image, tform, output_shape=(out_rows, out_cols))
+        # import pdb; pdb.set_trace()
 
-        # fig, ax = plt.subplots()
+        slope_percent = 100*slope
+        deg = np.degrees(np.arctan(-1*slope))
+        out = scipy.ndimage.interpolation.rotate(out, deg)
+        # out = scipy.ndimage.interpolation.rotate(out, 45)
+        # out = scipy.ndimage.interpolation.rotate(out, 45)
 
 
-        # if show_image:
-        #     ax.imshow(out)
-        #     # ax.plot(tform.inverse(src)[:, 0], tform.inverse(src)[:, 1], '.b') # plots the dots
-        #     ax.axis((0, out_cols, out_rows, 0))
-        #     plt.show()
+        if show_image:
+            fig, ax = plt.subplots()
+            ax.imshow(out)
+            # ax.plot(tform.inverse(src)[:, 0], tform.inverse(src)[:, 1], '.b') # plots the dots
+            ax.axis((0, out_cols, out_rows, 0))
+            plt.show()
 
-        converted = scipy.misc.toimage(out)
+        try:
+            converted = scipy.misc.toimage(out)
+        except Exception as e:
+            import pdb; pdb.set_trace()
 
         # import pdb; pdb.set_trace()
         # paper.paste(converted, (0,0))
         paper.paste(converted, (0, 0), converted)
-        # if show_image:
-        #     converted.show()
+        if show_image:
+            converted.show()
+            # paper.show()
 
         return paper
 
