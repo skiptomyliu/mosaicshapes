5d1c529a2816e851f790e330fedc9a1333dd3b5d
Dean Liu <deanliu@gmail.com>
2016-12-11 21:57:12 -0800

    Working through kmeans

--- colorpalette.py
+++ colorpalette.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage import io, feature
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin
 from sklearn.datasets import load_sample_image
@@ -12,14 +13,15 @@ from time import time
 class ColorPalette():
     
     def __init__(self):
+        self.colorbook = None
+        self.kmeans = None
+        self.labels = None
         pass
 
 
 
-    
+    def quantize(self, image_path, n_colors=64):
 
-
-    def quantize(self, image_path):
         def recreate_image(codebook, labels, w, h):
             """Recreate the (compressed) image from the code book & labels"""
             d = codebook.shape[1]
@@ -31,8 +33,29 @@ class ColorPalette():
                     label_idx += 1
             return image
 
-        n_colors = 64
 
+        sample_image = io.imread(image_path) 
+
+        # Convert to floats instead of the default 8 bits integer coding. Dividing by
+        # 255 is important so that plt.imshow behaves works well on float data (need to
+        # be in the range [0-1])
+        sample_image = np.array(sample_image, dtype=np.float64)/255
+
+        w, h, d = original_shape = tuple(sample_image.shape)
+        assert d == 3
+        image_array = np.reshape(sample_image, (w*h, d))
+
+        # xxx:  use percentage of total pixs instead?
+        image_array_sample = shuffle(image_array, random_state=0)[:2000] 
+        self.kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+        self.colorbook = self.kmeans.cluster_centers_
+
+        # Get labels for all points in the image
+        labels = self.kmeans.predict(image_array)
+
+
+
+        n_colors = 64
         # Load the Summer Palace photo
         china = load_sample_image("china.jpg")
 
@@ -68,6 +91,9 @@ class ColorPalette():
           image_array,
           axis=0)
         print("done in %0.3fs." % (time() - t0))
+
+
+
         # import pdb; pdb.set_trace()
         
         # Display all results, alongside original image
@@ -94,7 +120,16 @@ class ColorPalette():
         # plt.show()
 
 
+    # Takes in tuple color, converts to array.
+    def translate_color(self, color):
+        color = np.asarray(color)/float(255)
+        color = color.reshape(1,-1)
+
+        label = self.kmeans.predict(np.asarray(color))
+        print label
+        import pdb; pdb.set_trace()
+        self.colorbook[label]
+
         pass
 
-    def translate_color(self, color):
-        pass
+
