44a40d1bb1abf3331ab8febc03d75be99ce2a12b
Dean Liu <deanliu@gmail.com>
2017-01-04 18:09:58 -0800

    Changing generator in rmsdiff to list

--- util.py
+++ util.py
@@ -9,7 +9,7 @@ def rmsdiff(im1, im2):
     im2 = im2.convert("RGBA")
     diff = ImageChops.difference(im1, im2)
     h = diff.histogram()
-    sq = (value*((idx%256)**2) for idx, value in enumerate(h))
+    sq = [value*((idx%256)**2) for idx, value in enumerate(h)]
     sum_of_squares = sum(sq)
     rms = math.sqrt(sum_of_squares/float(im1.size[0] * im1.size[1]))
 
