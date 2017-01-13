f8dd34efb1cd59d47ff7221722d42ce1a0a3c70d
Dean Liu <deanliu@gmail.com>
2017-01-12 18:46:41 -0800

    lego

--- lego.py
+++ lego.py
@@ -17,6 +17,7 @@ def crop(im,height,width):
     imgwidth, imgheight = im.size
     rows = int(math.ceil(imgheight/float(height)))
     cols = int(math.ceil(imgwidth/float(width)))
+
     for i in range(rows):
         for j in range(cols):
             box = (
@@ -24,8 +25,7 @@ def crop(im,height,width):
                 i*height, 
                 util.clamp_int((j+1)*width, 0, imgwidth), 
                 util.clamp_int((i+1)*height, 0, imgheight)
-                )
-
+            )
             yield im.crop(box)
 
 
@@ -45,24 +45,24 @@ class Lego():
         return files
 
     @staticmethod
-    def stitch(file_paths, cols, rows, size):
+    def stitch(file_paths, cols, size):
         og = Image.new('RGB', size, 255)
         col,row = 0,0
         cur_w, cur_h = 0,0
         
-
         for f in file_paths:
             im = Image.open(f)
             w,h = im.size
             og.paste(im, (cur_w,cur_h))
-            col = (col + 1)%cols
+
+            col = (col+1)%cols
             cur_w += w
             if col==0:
                 row += 1
                 cur_h += h
-                cur_w,col = 0,0
-                og.show()
-                import pdb; pdb.set_trace()
+                cur_w = 0
+                # og.show()
+                # import pdb; pdb.set_trace()
 
         return og
                     
