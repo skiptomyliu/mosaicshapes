a0b3eb6544bfdaec0c7ff73764f660119ea2902a
Dean Liu <deanliu@gmail.com>
2017-04-05 20:41:53 -0700

    refactor rmsdiff

--- test/test_grid.py
+++ test/test_grid.py
@@ -10,7 +10,7 @@ import cProfile
 class TestGrid(unittest.TestCase):
     def setUp(self):
         self.grid = Grid("/Users/dean/Desktop/og/bo.jpg", pix_multi=.015, diamond=True, colorful=True, 
-            working_res=1024, enlarge=1024)
+            working_res=1024, enlarge=1600)
        
     def tearDown(self):
         pass
