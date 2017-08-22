85c007f44761bd7acffde7c7e1662ffe5c2c1fa5
Dean Liu <deanliu@gmail.com>
2017-08-21 20:16:06 -0700

    whitespace

--- trianglecell.py
+++ trianglecell.py
@@ -1,6 +1,5 @@
 
 
-
 from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 from colorpalette import ColorPalette
@@ -18,15 +17,16 @@ from cell import Cell, Quadrant
 xxx: 3/25, shrink is not implemented
 """
 
+
 class TriangleCell(Cell):
-    def __init__(self, size=(200,200), base_colors=[], second_colors=[], 
-        shrink=0, quadrant=Quadrant.top_left):
+    def __init__(self, size=(200, 200), base_colors=[], second_colors=[],
+                 shrink=0, quadrant=Quadrant.top_left):
 
         self.width = size[0]
         self.height = size[1]
 
-        self.colors = base_colors #Cell.gen_colors(base_color, n, colorful)
-        self.colors_secondary = second_colors#Cell.gen_colors(second_color,sn, colorful)
+        self.colors = base_colors
+        self.colors_secondary = second_colors
 
         self.quadrant = quadrant
         self.shrink = shrink
@@ -35,33 +35,34 @@ class TriangleCell(Cell):
     def __avg_lum(colors):
         cur_lum = 0
         for color in colors:
-             cur_lum += util.luminance(color[0], color[1], color[2])
+            cur_lum += util.luminance(color[0], color[1], color[2])
 
         return round(cur_lum / len(colors), 2)
 
     def avg_lum(self):
         return TriangleCell.__avg_lum(self.colors)
 
-
     @staticmethod
     def find_best(img, base_colors=[], second_colors=[], N=2):
-        color_combos = [[second_colors,base_colors], [base_colors, second_colors]]
+        color_combos = [[second_colors, base_colors], [base_colors, second_colors]]
         # color_combos = [[second_colors,base_colors]]
         quads = [Quadrant.top_left, Quadrant.top_right, Quadrant.bottom_left, Quadrant.bottom_right]
 
-        w,h=img.size
+        w, h = img.size
         best_img = None
         best_score = 10000
         for quad in quads:
             for color_combo in color_combos:
-                trect = TriangleCell(size=(w,h), 
-                    base_colors=color_combo[0], second_colors=color_combo[1], 
-                    shrink=0, quadrant=quad)
+                trect = TriangleCell(size=(w, h),
+                                     base_colors=color_combo[0],
+                                     second_colors=color_combo[1],
+                                     shrink=0,
+                                     quadrant=quad)
 
                 timg = trect.draw(N=1)
                 score = util.rmsdiff(img, timg)
                 if score <= best_score:
-                    best_img = trect#.draw(N=N)
+                    best_img = trect
                     best_score = score
 
         return best_img.draw(N=N), best_score
@@ -74,7 +75,7 @@ class TriangleCell(Cell):
         pw = int(round(.5 * .5 * shortest * 1/(len(self.colors) + len(self.colors_secondary))))
         pw = util.clamp_int(pw, 1, 10000)
         # pw = 6
-        # print pw 
+        # print pw
         # import pdb; pdb.set_trace()
         # pw = 1
         # print pw
@@ -96,40 +97,30 @@ class TriangleCell(Cell):
         y_offset = pw*(len(self.colors_secondary))
 
         for idx, color in enumerate(self.colors):
-            color = int(color[0]),int(color[1]),int(color[2])
-            width,height = n_width-pw*idx, n_height-pw*idx
+            color = int(color[0]), int(color[1]), int(color[2])
+            width, height = n_width-pw*idx, n_height-pw*idx
             sx = (pw*idx + x_offset)
             sy = (pw*idx + y_offset)
 
             # sx = int(round(len(self.colors)*pw/2.0))
             # sx += (pw*idx)
             ex = n_width - sx
-            coord = [(sx + pw*idx), sy, 
-                    (ex, sy), 
-                    (ex, (height-sy-idx))]        
+            coord = [(sx + pw*idx),
+                     sy,
+                     (ex, sy),
+                     (ex, (height-sy-idx))]
             canvas.polygon(coord, fill=color)
-
-            
-
-        # paper=ImageOps.mirror(paper)
 
         if self.quadrant == Quadrant.top_right:
             pass
-        elif self.quadrant ==  Quadrant.top_left:
+        elif self.quadrant == Quadrant.top_left:
             paper = paper.rotate(90)
-        elif self.quadrant ==  Quadrant.bottom_right:
+        elif self.quadrant == Quadrant.bottom_right:
             paper = paper.rotate(-90)
-        elif self.quadrant ==  Quadrant.bottom_left:
+        elif self.quadrant == Quadrant.bottom_left:
             paper = paper.rotate(180)
-        
+
         del canvas
-        # paper.thumbnail((self.width, self.height)) 
+        # paper.thumbnail((self.width, self.height))
 
         return paper
-
-
-
-
-
-
-
