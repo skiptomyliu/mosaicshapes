c311fd3c6f5d47f0383a71e7ee819be2ac70b8ee
Dean Liu <deanliu@gmail.com>
2018-03-10 06:35:33 -0800

    Removing more unneeded files related to the macapp

--- make.sh
+++
@@ -1,2 +0,0 @@
-python /Users/dean/playground/pyinstaller/pyinstaller.py run.py --icon=icon-windowed.icns --clean --windowed  --onefile --osx-bundle-identifier "com.mosaic.mosaicshapes"
-cp -rv /Users/dean/playground/vacuum/shapes/dist/run.app /Users/dean/playground/mosaicshapes/mosaicshapes/resources
